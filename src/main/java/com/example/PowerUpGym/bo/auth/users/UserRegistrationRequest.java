2621179ee27764e8343ea342b788277272430d34
Mohamad Samara <mohamadsamara1211@gmail.com>
2023-10-10 02:03:58 +0300

    test

--- src/main/java/com/example/PowerUpGym/bo/auth/users/UserRegistrationRequest.java
+++ src/main/java/com/example/PowerUpGym/bo/auth/users/UserRegistrationRequest.java
@@ -1,12 +1,9 @@
 package com.example.PowerUpGym.bo.auth.users;
 
 import com.example.PowerUpGym.Validation.FullName;
-import com.example.PowerUpGym.Validation.ValidPassword;
 import com.example.PowerUpGym.enums.Role;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
 @Getter
@@ -19,11 +16,23 @@ public class UserRegistrationRequest {
     @NotEmpty(message = "Full name must not be empty")
     @FullName(message = "Full name must be between 3-20")
     private String fullName;
+
+    @NotEmpty(message = "username must not be empty")
+    @FullName(message = "username must be between 3-20")
     private String username;
+
+    @NotEmpty(message = "email must not be empty")
+    @FullName(message = "email must be between 3-20")
     private String email;
+
+    @NotEmpty(message = "phoneNumber must not be empty")
+    @FullName(message = "phoneNumber must be between 3-20")
     private String phoneNumber;
-    @ValidPassword
+
+    @NotEmpty(message = "password must not be empty")
+    @FullName(message = "password must be between 3-20")
     private String password;
+
     private String image;
     private Role role;
 }
